a61d3a1407cc7225707b5338db0f51e4385daa1c
Codeur16 <charlesnjiosseu@gmail.com>
2024-01-13 01:30:22 +0100

    ok + deployment

--- Urgence/src/config/adresse.js
+++ Urgence/src/config/adresse.js
@@ -1,6 +1,6 @@
-const adresseCommentaire1 ="http://commentaire:3006" ; 
-const adresseNotification1= "http://notification:3004"
+const adresseCommentaire ="http://commentaire:3006" ; 
+const adresseNotification= "http://notification:3004"
 
-const adresseCommentaire ="http://localhost:3006" ; 
-const adresseNotification= "http://localhost:3004"
+const adresseCommentaire1 ="http://localhost:3006" ; 
+const adresseNotification1= "http://localhost:3004"
 module.exports={ adresseNotification, adresseCommentaire }
--- commentaire/src/config/adresse.js
+++ commentaire/src/config/adresse.js
@@ -1,4 +1,4 @@
-const adresseNotification1= "http://notification:3004"
-const adresseNotification= "http://localhost:3004"
+const adresseNotification= "http://notification:3004"
+const adresseNotification1= "http://localhost:3004"
 
 module.exports={ adresseNotification }
